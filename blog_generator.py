3560c5ca429a740f0250c7df59ef978a21614c75
Daniel Boules <49803185+dcboules@users.noreply.github.com>
2024-04-03 18:33:50 +0000

    Need to wait now, added funds to openai api dashboard, waiting for changes to implement so i can test.

--- blog_generator.py
+++ blog_generator.py
@@ -4,3 +4,14 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 
 openai.api_key = openai_api_key
 
+def generate_blog(paragraph_topic):
+    response = openai.completions.create(
+        model = 'gpt-3.5-turbo-instruct',
+        prompt = 'Write a paragraph about the following topic. ' + paragraph_topic,
+        max_tokens = 400,
+        temperature = 0.3
+    )
+    retrieve_blog = response.choices[0].text
+    return retrieve_blog
+print(generate_blog('Why NYC is better than your city.'))
+
